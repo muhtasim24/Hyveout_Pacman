869c8d3c9b3d858ccd53870f02d01d616f14b6c1
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-01-08 12:16:23 -0500

    initial project setup

---
+++ script.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d') // 
+
+canvas.width = window.innerWidth // size of the window's width
+canvas.height = window.innerHeight // size of the window's height
